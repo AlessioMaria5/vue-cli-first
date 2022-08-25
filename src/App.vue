ea6e5c9fc2ec8ca7532aa73a70aa3be47c54de09
AlessioMaria5 <alessiomaria5@gmail.com>
2022-08-25 17:10:57 +0200

    2 Hero Version ( Text Out and Text In )

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="app">
-    
     <Hero />
     <MyTitle />
-
   </div>
 </template>
 
@@ -27,7 +25,8 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  // color: white; PER img al centro
+  color: grey
+  
 }
 </style>
--- src/components/MyTitle.vue
+++ src/components/MyTitle.vue
@@ -10,5 +10,9 @@ export default {
 </script>
 
 <style>
+    h1 {
 
+        /* text-align: end;
+        margin-right: 30px; con titolo al centro */
+    }
 </style>
